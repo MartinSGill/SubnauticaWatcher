a018b31ba07647d23b12f989d2c0d893938366d9
Martin Gill <martin@martinsgill.co.uk>
2017-11-12 15:33:04 +0100

    Beginnings of a proper build system

---
+++ build.ps1
@@ -0,0 +1,23 @@
+
+try {
+    Import-Module VSSetup
+} catch {
+    try {
+        Install-Module VSSetup
+        Import-Module VSSetup            
+    }
+    catch {
+        Write-Error "Unable to import VSSetup module."
+        throw
+    }
+}
+
+$msbuildExe = @(Get-ChildItem -Recurse -Path ((Get-VSSetupInstance).InstallationPath) -Filter "msbuild.exe" -ErrorAction Stop)[0].Fullname
+
+if ($args.Count -gt 0) {
+	$cmdline = @($args)
+} else {
+	$cmdline = @("/v:d", "subnautica-watcher.proj")
+}
+
+& $msbuildExe $cmdline
