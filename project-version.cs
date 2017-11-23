28b6f115a862d8720e59e4315ff3bf7207feca75
Martin Gill <martin@martinsgill.co.uk>
2017-11-23 23:30:56 +0100

    updated version number for next release

--- project-version.cs
+++ project-version.cs
@@ -11,5 +11,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
